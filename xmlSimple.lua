90b8ce9bf3502b02bb8486e4658982e3f831d900
zhaorulysy <zhaorulysy@163.com>
2017-12-01 10:23:22 +0800

    Update xmlSimple.lua

--- xmlSimple.lua
+++ xmlSimple.lua
@@ -1,125 +1,9 @@
-module(..., package.seeall)
+local  XmlParser = {};
 
----------------------------------------------------------------------------------
----------------------------------------------------------------------------------
---
--- xml.lua - XML parser for use with the Corona SDK.
---
--- version: 1.2
---
--- CHANGELOG:
---
--- 1.2 - Created new structure for returned table
--- 1.1 - Fixed base directory issue with the loadFile() function.
---
--- NOTE: This is a modified version of Alexander Makeev's Lua-only XML parser
--- found here: http://lua-users.org/wiki/LuaXml
---
----------------------------------------------------------------------------------
----------------------------------------------------------------------------------
-function newParser()
-
-    XmlParser = {};
-
-    function XmlParser:ToXmlString(value)
-        value = string.gsub(value, "&", "&amp;"); -- '&' -> "&amp;"
-        value = string.gsub(value, "<", "&lt;"); -- '<' -> "&lt;"
-        value = string.gsub(value, ">", "&gt;"); -- '>' -> "&gt;"
-        value = string.gsub(value, "\"", "&quot;"); -- '"' -> "&quot;"
-        value = string.gsub(value, "([^%w%&%;%p%\t% ])",
-            function(c)
-                return string.format("&#x%X;", string.byte(c))
-            end);
-        return value;
-    end
-
-    function XmlParser:FromXmlString(value)
-        value = string.gsub(value, "&#x([%x]+)%;",
-            function(h)
-                return string.char(tonumber(h, 16))
-            end);
-        value = string.gsub(value, "&#([0-9]+)%;",
-            function(h)
-                return string.char(tonumber(h, 10))
-            end);
-        value = string.gsub(value, "&quot;", "\"");
-        value = string.gsub(value, "&apos;", "'");
-        value = string.gsub(value, "&gt;", ">");
-        value = string.gsub(value, "&lt;", "<");
-        value = string.gsub(value, "&amp;", "&");
-        return value;
-    end
-
-    function XmlParser:ParseArgs(node, s)
-        string.gsub(s, "(%w+)=([\"'])(.-)%2", function(w, _, a)
-            node:addProperty(w, self:FromXmlString(a))
-        end)
-    end
-
-    function XmlParser:ParseXmlText(xmlText)
-        local stack = {}
-        local top = newNode()
-        table.insert(stack, top)
-        local ni, c, label, xarg, empty
-        local i, j = 1, 1
-        while true do
-            ni, j, c, label, xarg, empty = string.find(xmlText, "<(%/?)([%w_:]+)(.-)(%/?)>", i)
-            if not ni then break end
-            local text = string.sub(xmlText, i, ni - 1);
-            if not string.find(text, "^%s*$") then
-                local lVal = (top:value() or "") .. self:FromXmlString(text)
-                stack[#stack]:setValue(lVal)
-            end
-            if empty == "/" then -- empty element tag
-                local lNode = newNode(label)
-                self:ParseArgs(lNode, xarg)
-                top:addChild(lNode)
-            elseif c == "" then -- start tag
-                local lNode = newNode(label)
-                self:ParseArgs(lNode, xarg)
-                table.insert(stack, lNode)
-		top = lNode
-            else -- end tag
-                local toclose = table.remove(stack) -- remove top
-
-                top = stack[#stack]
-                if #stack < 1 then
-                    error("XmlParser: nothing to close with " .. label)
-                end
-                if toclose:name() ~= label then
-                    error("XmlParser: trying to close " .. toclose.name .. " with " .. label)
-                end
-                top:addChild(toclose)
-            end
-            i = j + 1
-        end
-        local text = string.sub(xmlText, i);
-        if #stack > 1 then
-            error("XmlParser: unclosed " .. stack[#stack]:name())
-        end
-        return top
-    end
-
-    function XmlParser:loadFile(xmlFilename, base)
-        if not base then
-            base = system.ResourceDirectory
-        end
-
-        local path = system.pathForFile(xmlFilename, base)
-        local hFile, err = io.open(path, "r");
-
-        if hFile and not err then
-            local xmlText = hFile:read("*a"); -- read file content
-            io.close(hFile);
-            return self:ParseXmlText(xmlText), nil;
-        else
-            print(err)
-            return nil
-        end
-    end
-
-    return XmlParser
-end
+--注意 plus只能保证同一个xml解析是顺序的。
+--层集之间的解析由于lua hash表的设计导致每次执行的plus值不一定一致
+XmlParser.plus = 1
+XmlParser.isPackage = false
 
 function newNode(name)
     local node = {}
@@ -134,7 +18,13 @@ function newNode(name)
     function node:setName(name) self.___name = name end
     function node:children() return self.___children end
     function node:numChildren() return #self.___children end
+	
     function node:addChild(child)
+		if not XmlParser.isPackage then
+			child["@plus"] = XmlParser.plus
+			XmlParser.plus = XmlParser.plus + 1
+		end	
+
         if self[child:name()] ~= nil then
             if type(self[child:name()].name) == "function" then
                 local tempTable = {}
@@ -167,3 +57,99 @@ function newNode(name)
 
     return node
 end
+
+function XmlParser:ToXmlString(value)
+	value = string.gsub(value, "&", "&amp;"); -- '&' -> "&amp;"
+	value = string.gsub(value, "<", "&lt;"); -- '<' -> "&lt;"
+	value = string.gsub(value, ">", "&gt;"); -- '>' -> "&gt;"
+	value = string.gsub(value, "\"", "&quot;"); -- '"' -> "&quot;"
+	value = string.gsub(value, "([^%w%&%;%p%\t% ])",
+		function(c)
+			return string.format("&#x%X;", string.byte(c))
+		end);
+	return value;
+end
+
+function XmlParser:FromXmlString(value)
+	value = string.gsub(value, "&#x([%x]+)%;",
+		function(h)
+			return string.char(tonumber(h, 16))
+		end);
+	value = string.gsub(value, "&#([0-9]+)%;",
+		function(h)
+			return string.char(tonumber(h, 10))
+		end);
+	value = string.gsub(value, "&quot;", "\"");
+	value = string.gsub(value, "&apos;", "'");
+	value = string.gsub(value, "&gt;", ">");
+	value = string.gsub(value, "&lt;", "<");
+	value = string.gsub(value, "&amp;", "&");
+	return value;
+end
+
+function XmlParser:ParseArgs(node, s)
+	string.gsub(s, "(%w+)=([\"'])(.-)%2", function(w, _, a)
+		node:addProperty(w, self:FromXmlString(a))
+	end)
+end
+
+function XmlParser:ParseXmlText(xmlText)
+	local stack = {}
+	local top = newNode()
+	table.insert(stack, top)
+	local ni, c, label, xarg, empty
+	local i, j = 1, 1
+	while true do
+		ni, j, c, label, xarg, empty = string.find(xmlText, "<(%/?)([%w_:]+)(.-)(%/?)>", i)
+		if not ni then break end
+		local text = string.sub(xmlText, i, ni - 1);
+		if not string.find(text, "^%s*$") then
+			local lVal = (top:value() or "") .. self:FromXmlString(text)
+			stack[#stack]:setValue(lVal)
+		end
+		if empty == "/" then -- empty element tag
+			local lNode = newNode(label)
+			self:ParseArgs(lNode, xarg)
+			top:addChild(lNode)
+		elseif c == "" then -- start tag
+			local lNode = newNode(label)
+			self:ParseArgs(lNode, xarg)
+			table.insert(stack, lNode)
+			top = lNode
+		else -- end tag
+			local toclose = table.remove(stack) -- remove top
+
+			top = stack[#stack]
+			if #stack < 1 then
+				error("XmlParser: nothing to close with " .. label)
+			end
+			if toclose:name() ~= label then
+				error("XmlParser: trying to close " .. tostring(toclose:name()) .. " with " .. label)
+			end
+			top:addChild(toclose)
+		end
+		i = j + 1
+	end
+	local text = string.sub(xmlText, i);
+	if #stack > 1 then
+		error("XmlParser: unclosed " .. stack[#stack]:name())
+	end
+	return top
+end
+
+function XmlParser:loadFile(path, base)
+	
+	local hFile, err = io.open(path, "r");
+
+	if hFile and not err then
+		local xmlText = hFile:read("*a"); -- read file content
+		io.close(hFile);
+		return self:ParseXmlText(xmlText), nil;
+	else
+		print(err)
+		return nil
+	end
+end
+
+return XmlParser
+
